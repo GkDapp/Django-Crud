22f503e2fce2bae43dd10c1c026e4fd543d09e91
GkDapp <ganeshkumar310300@gmail.com>
2024-07-31 08:34:06 +0530

    Added project files from another repository

--- crudproject/crudapp/views.py
+++ crudproject/crudapp/views.py
@@ -3,7 +3,7 @@ from .forms import OrderForm
 from .models import Orders
 
 # Create your views here.
-def orderFormView(request):
+def orderFormview(request):
     form = OrderForm()
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -14,13 +14,13 @@ def orderFormView(request):
     context = {'form': form}
     return render(request, template_name, context)
 
-def showView(request):
+def showview(request):
     obj = Orders.objects.all()
     template_name = 'crudapp/show.html'
     context = {'obj': obj}
     return render(request, template_name, context)
 
-def updateView(request, f_oid):
+def updateview(request, f_oid):
     obj = Orders.objects.get(oid=f_oid)
     form = OrderForm(instance=obj)
     if request.method == 'POST':
@@ -32,7 +32,7 @@ def updateView(request, f_oid):
     context = {'form': form}
     return render(request, template_name, context)
 
-def deleteView(request, f_oid):
+def deleteview(request, f_oid):
     obj = Orders.objects.get(oid=f_oid)
     if request.method == 'POST':
         obj.delete()
